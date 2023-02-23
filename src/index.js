d9d5e0b68e13c0fb9a43cab9445124e57e2a097a
vishnupal <vishnupalgehlot@gmail.com>
2023-02-23 19:57:24 +0530

    Completed city repository

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
-const CityRepository = require('./repository/city-repository');
 
 const setupAndStartServer = async () => {
   // create the express object
@@ -11,11 +10,8 @@ const setupAndStartServer = async () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server started at ${PORT}`);
-    const repo = new CityRepository();
-
-    repo.createCity({ name: 'New Delhi' });
   });
 };
 
