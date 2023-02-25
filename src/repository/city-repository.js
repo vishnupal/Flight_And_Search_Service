b7235497d70857714460b114f6eaa8335dd334f9
vishnupal <vishnupalgehlot@gmail.com>
2023-02-25 18:29:24 +0530

    minor fixes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -26,11 +26,16 @@ class CityRepository {
   async updateCity(cityId, data) {
     // data -> {name:"Prayagraj"}
     try {
-      const city = await City.update(data, {
-        where: {
-          id: cityId,
-        },
-      });
+      // this below code also work but it doesn't return updated object
+      // if we use with pgsql then below code also return updated object but need use retuning: true after where
+      // const city = await City.update(data, {
+      //   where: {
+      //     id: cityId,
+      //   },
+      // });
+      const city = await City.findByPk(cityId);
+      city.name = data.name;
+      await city.save();
       return city;
     } catch (error) {
       console.log('Something went wrong in the repository layer');
