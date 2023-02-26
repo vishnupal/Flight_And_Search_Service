2472b2852e03262541e50b487d177516370c590e
vishnupal <vishnupalgehlot@gmail.com>
2023-02-26 16:20:26 +0530

    Added airports model and do association

---
+++ src/migrations/20230226095803-create-airport.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Cities', // from Cities table
+          key: 'id', // Cities table id refer as cityId in this Airport migration file
+          as: 'cityId', // Cities table id refer as cityId here
+        },
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
+  },
+};
