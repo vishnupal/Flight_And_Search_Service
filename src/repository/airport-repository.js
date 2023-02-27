163078c65471465afead29c856d44b513bb98241
vishnupal <vishnupalgehlot@gmail.com>
2023-02-27 21:03:49 +0530

    Added filght-create request middleware

--- src/repository/airport-repository.js
+++ src/repository/airport-repository.js
@@ -5,6 +5,17 @@ class AirportRepository extends CrudRepository {
   constructor() {
     super(Airport);
   }
+  // we want customize method then override method here
+  // like
+  // async getAll(filter) {
+  //   try {
+  //     const result = await this.findAll({
+  //       where: {
+  //         filter,
+  //       },
+  //     });
+  //   } catch (error) {}
+  // }
 }
 
 module.exports = AirportRepository;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { validateCreateFlight } = require('../../middlewares/index');
 
 const city = require('./city/index');
 const flights = require('./flights/index');
 const airports = require('./airports/index');
 
 router.use('/cities', city);
-router.use('/flights', flights);
+router.use('/flights', validateCreateFlight, flights);
 router.use('/airports', airports);
 
 module.exports = router;
